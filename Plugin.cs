ca2b8a59b6c94ce38e386add97c581bb739c7d7d
onusai <alexsiryy1@gmail.com>
2024-08-12 09:50:27 -0500

    Fixed death and added sethp command

--- Plugin.cs
+++ Plugin.cs
@@ -37,21 +37,18 @@ namespace UHC
         {
             config = PluginConfig.Load("UHC");
             ServerApi.Hooks.GameInitialize.Register(this, OnGameLoad);
-            
-                
         }
 
         void OnGameLoad(EventArgs e)
         {
             if (!config.Enabled) return;
 
-            //ServerApi.Hooks.ServerJoin.Register(this, OnServerJoin);
-            //ServerApi.Hooks.ServerLeave.Register(this, OnServerLeave);
             TShockAPI.Hooks.PlayerHooks.PlayerPostLogin += OnPlayerPostLogin;
             ServerApi.Hooks.WorldSave.Register(this, OnWorldSave);
             GetDataHandlers.PlayerDamage += OnPlayerDamage;
             GetDataHandlers.PlayerHP += OnPlayerHP;
-            //RegisterCommand("ho", "", HealO, "");
+
+            RegisterCommand("sethp", "tshock.admin", CMDOnSetHP, "Used to set UHC player hp\nUsage: /sethp <amount> <player name>\nExample: /sethp 400 onusai");
         }
 
         protected override void Dispose(bool disposing)
@@ -62,8 +59,6 @@ namespace UHC
 
                 if (config.Enabled)
                 {
-                    //ServerApi.Hooks.ServerJoin.Deregister(this, OnServerJoin);
-                    //ServerApi.Hooks.ServerLeave.Deregister(this, OnServerLeave);
                     TShockAPI.Hooks.PlayerHooks.PlayerPostLogin -= OnPlayerPostLogin;
                     ServerApi.Hooks.WorldSave.Deregister(this, OnWorldSave);
                     GetDataHandlers.PlayerDamage -= OnPlayerDamage;
@@ -78,68 +73,60 @@ namespace UHC
             TShockAPI.Commands.ChatCommands.Add(new Command(perm, handler, name) { HelpText = helptext });
         }
 
-        void Print(string text)
+        void CMDOnSetHP(CommandArgs args)
         {
-            TShock.Utils.Broadcast(text, Color.White);
+            if (args.Parameters.Count != 2)
+            {
+                args.Player.SendErrorMessage("Usage: /sethp <amount> <player name>\nExample: /sethp 400 onusai\nExample: /sethp 300 \"corn stick\"");
+                return;
+            }
+
+            int amount = 0;
+            if (!int.TryParse(args.Parameters[0], out amount))
+            {
+                args.Player.SendErrorMessage(String.Format("Invalid HP amount: {0}", args.Parameters[0]));
+                return;
+            }
+            
+            if (config.PlayerHP.ContainsKey(args.Parameters[1]))
+            {
+                // in case you want to revive dead player
+                config.PlayerHP[args.Parameters[1]] = amount;
+            }
+
+            List<TSPlayer> players = TSPlayer.FindByNameOrID(args.Parameters[1]);
+            if (players.Count == 0)
+            {
+                args.Player.SendErrorMessage(String.Format("Unable to find player with username: {0}. Try surrounding the name in \"quotes\"", args.Parameters[1]));
+                return;
+            }
+
+            TSPlayer player = players[0];
+            SetHP(amount, player);
         }
 
         void OnPlayerHP(Object sender, GetDataHandlers.PlayerHPEventArgs e)
         {
-
             if (!e.Player.GetData<bool>("rdy")) return;
-            /*
-            
-            if (PlayerForceHP.ContainsKey(name))
-            {
-                Print("forced hp");
-                SetHP(PlayerForceHP[name], e.Player);
-                PlayerForceHP.Remove(name);
-                e.Current = (short)PlayerForceHP[name];
-                return;
-            }
-                        */
+
             string name = e.Player.Name;
             int uhp = config.PlayerHP[name];
 
-            //if (e.Current != uhp)
-            //{
-            //    SetHP(uhp, e.Player);
-            //}
-
-            
             if (e.Current > uhp)
-            {
-                if (uhp <= 0)
-                {
-                    uhp = e.Player.TPlayer.statLifeMax;
-                    config.PlayerHP[name] = uhp;
-                }
                 SetHP(uhp, e.Player);
-                //Print(String.Format("khp: {0} -> {1}", e.Current, uhp));
-                
-            }
             else
-            {
-                //Print(String.Format("hp: {0}", e.Current));
-                config.PlayerHP[name] = e.Current;
-                //SetHP(e.Current, e.Player);
-            }
+                config.PlayerHP[name] = Math.Clamp(e.Current, 0, e.Player.TPlayer.statLifeMax);
             
-
-           // e.Handled = true;
-
-            //Print(String.Format("hp - {0}", e.Current));
-
         }
 
         void OnPlayerDamage(Object sender, GetDataHandlers.PlayerDamageEventArgs e)
         {
-            config.PlayerHP[e.Player.Name] -= e.Damage;
+            config.PlayerHP[e.Player.Name] -= Math.Clamp(e.Damage, 0, e.Player.TPlayer.statLifeMax);
         }
 
         void OnPlayerPostLogin(PlayerPostLoginEventArgs e)
         {
-            TSPlayer player = e.Player;// TShock.Players[e.Who];
+            TSPlayer player = e.Player;
             if (player == null) return;
 
             int hp = 0;
@@ -149,24 +136,16 @@ namespace UHC
             else
                 hp = player.TPlayer.statLife;
 
+            if (hp <= 0)
+            {
+
+                player.Kick("You are dead", true, true);
+                return;
+            }
+
             SetHP(hp, player);
-
-
             player.SetData<bool>("rdy", true);
-
-            //Print(String.Format("joined {0} hp: {1}", player.Name, hp));
-
         }
-        void OnServerJoin(JoinEventArgs e)
-        {
-
-        }
-
-        void OnServerLeave(LeaveEventArgs e)
-        {
-
-        }
-
 
         void OnWorldSave(WorldSaveEventArgs e)
         {
@@ -175,15 +154,10 @@ namespace UHC
 
         void SetHP(int hp, TSPlayer player)
         {
+            hp = Math.Clamp(hp, 0, player.TPlayer.statLifeMax);
             config.PlayerHP[player.Name] = hp;
             player.TPlayer.statLife = hp;
             NetMessage.SendData((int)PacketTypes.PlayerHp, -1, -1, null, player.TPlayer.whoAmI);
-            //Print(String.Format("sethp: {0}", hp));
-        }
-
-        void HealO(CommandArgs e)
-        {
-            SetHP(e.TPlayer.statLifeMax, e.Player);
         }
 
         public static class PluginConfig
